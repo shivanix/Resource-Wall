0621fc52a6263e5934c46fa16d706f59a5cccd42
shivanix <shvni.here@gmail.com>
2021-07-27 20:20:17 +0000

    slight modifications for login and register pages

--- routes/register.js
+++ routes/register.js
@@ -15,44 +15,56 @@ module.exports = (db) => {
   router.post("/", (req, res) => {
     const user = req.body;
     console.log("THIS IS THE USER: ", user);
-
+    let userAlreadyExists = false;
     db.query(`SELECT * FROM users
     WHERE email = $1
     OR username = $2`, [user.email, user.username])
     .then((result) => {
-
+      console.log("Rows result: ",result.rows[0]);
+      //If user with same email or username was found
       if(result.rows[0]){
+        userAlreadyExists = true;
         console.log("USER ALREADY EXISTS #####################");
         res.status(400).send('User already exists');
         return null;
-      };
-
+      }
+      //If no user with same email or username was found
+      else{
+        createNewUser(user, req, res);
+      }
     })
     .catch((err) => {
       console.log(err.message);
       return null;
     });
 
-    db.query(`
-      INSERT INTO users(email, username, password)
-      VALUES ($1, $2, $3)
-      RETURNING *;
-      `, [user.email, user.username, user.password])
-    .then((data) => {
-      const newUser = data.rows[0];
-      console.log("THIS IS NEWUSER: ". newUser) // getting undefined here
 
-      if (!newUser) {
-        res.send("Error: ", error.message);
-      } else {
-        req.session.user_id = newUser;
-        console.log("##############", newUser)
-        res.redirect("/home");
-      }
-    })
-    .catch((error) => {
-      res.status(400).send(error.message);
-    })
   })
+
+  const createNewUser = (user, req, res)=>{
+    db.query(`
+        INSERT INTO users(email, username, password)
+        VALUES ($1, $2, $3)
+        RETURNING *;
+        `, [user.email, user.username, user.password])
+      .then((data) => {
+        const newUser = data.rows[0];
+        console.log("THIS IS NEWUSER: ". newUser) // getting undefined here
+
+        if (!newUser) {
+          res.send("Error: ", error.message);
+        } else {
+          req.session.user_id = newUser;
+          console.log("##############", newUser)
+          res.redirect("/home");
+        }
+      })
+      .catch((error) => {
+        res.status(400).send(error.message);
+      })
+  }
+
   return router;
 };
+
+
