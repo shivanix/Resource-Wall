64b2717280a3219f67b38251e80744793c510cc4
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-27 14:23:21 +0000

    edit my-resources route

--- routes/my-resources.js
+++ routes/my-resources.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const router  = express.Router();
-const resourceQueries = require('../db/')
-
-// GET /my-resources
-router.get('/', (req, res) => {
-  resourceQueries.getResources()
-    .then((resources) => {
-      res.json(resources);
-    });
-})
+const resourceQueries = require('../db/resource-queries')
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    res.render("my-resources");
-      })
+// GET /my-resources
+router.get('/', (req, res) => {
+//   resourceQueries.getResources()
+//     .then((resources) => {
+//       res.json(resources);
+//     });
+// console.log('res', res)
+// console.log('req', req)
+const templateVars = { title: 'Healthy Breakfast Options' }
+  res.render("my-resources", templateVars);
+})
+
+// GET /my-resources/:id
+// router.get('/:id', (req, res) => {
+//   resourceQueries.getResourcesById(req.params.id)
+//     .then((resources) => {
+//       res.json(resources);
+//     });
+// });
 
   return router;
 };
