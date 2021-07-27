ab49e053792be5262c61a5c447342f61b93817f3
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-27 14:19:53 +0000

    add getResources and getResourcesById queries

--- db/resource-queries.js
+++ db/resource-queries.js
@@ -1,14 +1,15 @@
-const db = require('./db');
+const db = require('../lib/db');
 
 const getResources = () => {
-  return db.query('SELECT * FROM resources;')
+  return db.query(`
+  SELECT * FROM resources`)
     .then((response) => {
       return response.rows;
     });
 };
 
 const getResourcesById = (id) => {
-  return db.query('SELECT * FROM resources WHERE id = $1, [id]')
+  return db.query(`SELECT * FROM resources WHERE id = $1`, [id])
     .then((response) => {
       return response.rows[0];
     });
