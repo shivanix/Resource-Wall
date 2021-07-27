422de8f88bca90bc6948eac456505c11215c69f4
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-27 14:25:23 +0000

    create tags seeds

---
+++ db/seeds/tags.sql
@@ -0,0 +1,6 @@
+INSERT INTO tags(resource_id, name) VALUES (1, 'Healthy Living');
+INSERT INTO tags(resource_id, name) VALUES (1, 'Breakfast');
+INSERT INTO tags(resource_id, name) VALUES (1, 'Smoothies');
+INSERT INTO tags(resource_id, name) VALUES (2, 'Paint');
+INSERT INTO tags(resource_id, name) VALUES (2, 'DIY');
+INSERT INTO tags(resource_id, name) VALUES (2, 'Gravity');
