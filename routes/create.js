19ee748b76c0dd69f3593f56dbb14cac1478e578
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-27 19:06:27 -0400

    Added a post route to /create which will add a new resource into the resources database, each resource will be accessed individually through their ID, resource/ newResource.id

--- routes/create.js
+++ routes/create.js
@@ -3,14 +3,32 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-
     const user = req.session.user_id;
     const templateVars = {
       username: user.username
     };
-
     res.render("create", templateVars);
       })
 
+  router.post("/", (req, res) => {
+    const resource = req.body;
+    console.log("RESOURCE BODY AKA REQ.BODY:  ", resource)
+
+    db.query(`
+      INSERT INTO resources(category, title, thumbnail_photo_url, summary, description, tag_1, tag_2, tag_3, resource_url)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      RETURNING *;
+      `, [resource.category, resource.title, resource.photo_url, resource.summary, resource.description, resource.tags[0], resource.tags[1], resource.tags[2], resource.resource_url])
+    .then((data) => {
+      const newResource = data.rows[0];
+      console.log("THIS IS NEW RESOURCE CREATED:  ", newResource)
+
+      req.session.resource = newResource;
+      res.redirect(`/resource/${newResource.id}`);
+      })
+    .catch((error) => {
+      res.status(400).send(error.message);
+    })
+  })
   return router;
 };
