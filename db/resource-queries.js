19644142ef99cc448691c682cca9d6cae31e2dec
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-27 01:31:12 +0000

    create getResource and getResourceById queries

---
+++ db/resource-queries.js
@@ -0,0 +1,20 @@
+const db = require('./db');
+
+const getResources = () => {
+  return db.query('SELECT * FROM resources;')
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const getResourcesById = (id) => {
+  return db.query('SELECT * FROM resources WHERE id = $1, [id]')
+    .then((response) => {
+      return response.rows[0];
+    });
+}
+
+module.exports = {
+  getResources,
+  getResourcesById
+}
