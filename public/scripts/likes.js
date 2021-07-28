03fdc2e2be2af56042290d46e147d7cdeb4772c9
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-28 18:26:01 -0400

    Commit before I pull recent changes

---
+++ public/scripts/likes.js
@@ -0,0 +1,16 @@
+/* When the button is clicked, is_liked will be set to true and added to the likes database -- kind of a query? */
+$(() => {
+  $(".like-button").click(function() {
+    alert("HELLLLLLLOOOOOOOOOO")
+    $(this).toggleClass(".is-liked");
+
+    if ($(this).hasClass(".is-liked")) {
+      val++
+      //userID and resourceID should be enter into the likes table (db) --liking the resource(by clicking the button)
+    } else {
+      val--
+      //userID and resourceID should be deleted from the likes table (db) --removing a like (by clicking again)
+    }
+    $(this).text(val);
+  });
+});
