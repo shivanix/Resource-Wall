a6352851deba0dac29d676b89c0cd22990818c3a
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-28 12:43:39 -0400

    Removed req.session.resource in /:id GET route becuse it was interfereing with how logged in users can view each resource and replaced it with req.session.user_id

--- routes/create.js
+++ routes/create.js
@@ -12,18 +12,20 @@ module.exports = (db) => {
 
   router.post("/", (req, res) => {
     const resource = req.body;
-    console.log("RESOURCE BODY AKA REQ.BODY:  ", resource)
+    // console.log("RESOURCE BODY AKA REQ.BODY:  ", resource)
+    let owner = req.session.user_id
+    console.log("THIS IS THE OWNER.ID: ######", owner.id)
 
     db.query(`
-      INSERT INTO resources(category, title, thumbnail_photo_url, summary, description, tag_1, tag_2, tag_3, resource_url)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      INSERT INTO resources(owner_id, category, title, thumbnail_photo_url, summary, description, tag_1, tag_2, tag_3, resource_url)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
       RETURNING *;
-      `, [resource.category, resource.title, resource.photo_url, resource.summary, resource.description, resource.tags[0], resource.tags[1], resource.tags[2], resource.resource_url])
+      `, [owner.id, resource.category, resource.title, resource.photo_url, resource.summary, resource.description, resource.tags[0], resource.tags[1], resource.tags[2], resource.resource_url])
     .then((data) => {
       const newResource = data.rows[0];
       console.log("THIS IS NEW RESOURCE CREATED:  ", newResource)
 
-      req.session.resource = newResource;
+      // req.session.resource = newResource;
       res.redirect(`/resource/${newResource.id}`);
       })
     .catch((error) => {
