943cb6ffd5d3f2891c5b7d069714949d7dc30fdc
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-29 18:30:38 +0000

    add functionality for all category queries

--- routes/searched-router.js
+++ routes/searched-router.js
@@ -18,5 +18,75 @@ module.exports = (db) => {
       });
   });
 
+  router.get("/food-and-drink", (req, res) => {
+    db.query(`SELECT * FROM resources
+    WHERE category = $1;`, ['Food and Drink'])
+      .then(results => {
+        const resources = results.rows;
+        res.json( resources );
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/arts-and-crafts", (req, res) => {
+    db.query(`SELECT * FROM resources
+    WHERE category = $1;`, ['Arts and Crafts'])
+      .then(results => {
+        const resources = results.rows;
+        res.json( resources );
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/education", (req, res) => {
+    db.query(`SELECT * FROM resources
+    WHERE category = $1;`, ['Education'])
+      .then(results => {
+        const resources = results.rows;
+        res.json( resources );
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/animals", (req, res) => {
+    db.query(`SELECT * FROM resources
+    WHERE category = $1;`, ['Animals'])
+      .then(results => {
+        const resources = results.rows;
+        res.json( resources );
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/other", (req, res) => {
+    db.query(`SELECT * FROM resources
+    WHERE category = $1;`, ['Other'])
+      .then(results => {
+        const resources = results.rows;
+        res.json( resources );
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   return router;
 };
