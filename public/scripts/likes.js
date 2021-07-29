bb2b2319ecec3fd6a069130d04176cb83824e3d2
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-28 22:06:24 -0400

    AJAX for the like and unlike button

--- public/scripts/likes.js
+++ public/scripts/likes.js
@@ -1,16 +1,20 @@
 /* When the button is clicked, is_liked will be set to true and added to the likes database -- kind of a query? */
 $(() => {
   $(".like-button").click(function() {
-    alert("HELLLLLLLOOOOOOOOOO")
-    $(this).toggleClass(".is-liked");
 
-    if ($(this).hasClass(".is-liked")) {
-      val++
-      //userID and resourceID should be entered into the likes table (db) --liking the resource(by clicking the button)
-    } else {
-      val--
-      //userID and resourceID should be deleted from the likes table (db) --removing a like (by clicking again)
-    }
-    $(this).text(val);
+    $(this).toggleClass(".like-button");
+    const $heartIcon = $(".like-button > i");
+    const val = $heartIcon[0].className.includes("far") ? "like" : "unlike";
+
+    $.ajax({
+      method: "POST",
+      url: `${window.location.pathname}/${val}`
+    }).done((like) => {
+      if (like) {
+        $heartIcon.removeClass("far").addClass("fas")
+      } else {
+        $heartIcon.removeClass("fas").addClass("far")
+      }
+    });
   });
 });
