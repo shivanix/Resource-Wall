c41180b0ea114f3afcbd4df1af41e3b5ad687c3c
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-29 18:30:15 +0000

    add functionality for all category queries

--- routes/search.js
+++ routes/search.js
@@ -5,41 +5,77 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-  // router.get("/", (req, res) => {
-
-  //   const user = req.session.user_id;
-
-  //   const templateVars = {
-  //     username: user.username,
-  //     id: user.id
-  //   };
-
-  //   res.render("search", templateVars);
-  //     })
-
   router.get(`/music`, (req, res) => {
     const user = req.session.user_id;
-    console.log('SOMETHING')
+    // const category = ;
     const templateVars = {
       username: user.username,
-      id: user.id
+      id: user.id,
+      // category
     };
 
     res.render("search", templateVars);
   })
 
-  // router.post("/music", (req, res) => {
+  router.get(`/food-and-drink`, (req, res) => {
+    const user = req.session.user_id;
+    // const category = ;
+    const templateVars = {
+      username: user.username,
+      id: user.id,
+      // category
+    };
 
-  //   const user = req.session.user_id;
+    res.render("search", templateVars);
+  })
 
-  //   const templateVars = {
-  //     username: user.username,
-  //     id: user.id
-  //   };
+  router.get(`/arts-and-crafts`, (req, res) => {
+    const user = req.session.user_id;
+    // const category = ;
+    const templateVars = {
+      username: user.username,
+      id: user.id,
+      // category
+    };
 
-  //   res.render("search", templateVars);
-  //     })
+    res.render("search", templateVars);
+  })
 
+  router.get(`/education`, (req, res) => {
+    const user = req.session.user_id;
+    // const category = ;
+    const templateVars = {
+      username: user.username,
+      id: user.id,
+      // category
+    };
+
+    res.render("search", templateVars);
+  })
+
+  router.get(`/animals`, (req, res) => {
+    const user = req.session.user_id;
+    // const category = ;
+    const templateVars = {
+      username: user.username,
+      id: user.id,
+      // category
+    };
+
+    res.render("search", templateVars);
+  })
+
+  router.get(`/other`, (req, res) => {
+    const user = req.session.user_id;
+    // const category = ;
+    const templateVars = {
+      username: user.username,
+      id: user.id,
+      // category
+    };
+
+    res.render("search", templateVars);
+  })
 
   return router;
 }
