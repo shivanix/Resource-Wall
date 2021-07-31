fb99061e1315a91fd57dd41d557bd7f8fc1e303d
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-31 02:48:45 +0000

    alphabetize and fixed url character cases

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -14,32 +14,105 @@ $(() => {
         ${resource.summary}
       </div>
       <div class='category'>
-        ${resource.category}
+      ${resource.category}
       </div>
       <div class='tags-container'>
-        <div class='tag'>
-          #${resource.tag_1}
-        </div>
-        <div class='tag'>
-          #${resource.tag_2}
-        </div>
-        <div class='tag'>
-          #${resource.tag_3}
-        </div>
+      <div class='tag'>
+      #${resource.tag_1}
+      </div>
+      <div class='tag'>
+      #${resource.tag_2}
+      </div>
+      <div class='tag'>
+      #${resource.tag_3}
+      </div>
       </div>
       <div class='read-more'>
-        <a href='/resource/${resource.id}' class='read-link'>
-        READ MORE
-        </a>
+      <a href='/resource/${resource.id}' class='read-link'>
+      READ MORE
+      </a>
       </div>
-    </div>
-    `);
+      </div>
+      `);
 
-    return $resource;
+      return $resource;
+    }
+
+    if (window.location.pathname === '/search/animals') {
+      $.get('/api/search/animals')
+      .then((resources) => {
+        renderAnimalsResources(resources);
+      })
+      .catch(err => {
+        console.log('err:', err.message)
+      })
+
+      const renderAnimalsResources = (resources) => {
+        const $animalsResourceContainer = $('#categorizedResources');
+
+        resources.reverse().forEach(resource => {
+          $animalsResourceContainer.append(createResource(resource));
+        })
+      };
+    }
+
+    if (window.location.pathname === '/search/arts-and-crafts') {
+      $.get('/api/search/arts-and-crafts')
+      .then((resources) => {
+        renderArtsAndCraftsResources(resources);
+      })
+      .catch(err => {
+        console.log('err:', err.message)
+      })
+
+      const renderArtsAndCraftsResources = (resources) => {
+        const $artsAndCraftsResourceContainer = $('#categorizedResources');
+
+        resources.reverse().forEach(resource => {
+          $artsAndCraftsResourceContainer.append(createResource(resource));
+        })
+      };
+    }
+
+
+    if (window.location.pathname === '/search/education') {
+      $.get('/api/search/education')
+      .then((resources) => {
+        renderEducationResources(resources);
+      })
+      .catch(err => {
+        console.log('err:', err.message)
+      })
+
+      const renderEducationResources = (resources) => {
+        const $educationResourceContainer = $('#categorizedResources');
+
+        resources.reverse().forEach(resource => {
+          $educationResourceContainer.append(createResource(resource));
+        })
+      };
+    }
+
+    if (window.location.pathname === '/search/food-and-drink') {
+      $.get('/api/search/food-and-drink')
+      .then((resources) => {
+        renderFoodAndDrinkResources(resources);
+      })
+      .catch(err => {
+        console.log('err:', err.message)
+      })
+
+      const renderFoodAndDrinkResources = (resources) => {
+        const $foodAndDrinkResourceContainer = $('#categorizedResources');
+
+        resources.reverse().forEach(resource => {
+          $foodAndDrinkResourceContainer.append(createResource(resource));
+        })
+    };
   }
 
-  if (window.location.pathname === '/search/Music') {
-  $.get('/api/search/Music')
+    if (window.location.pathname === '/search/music') {
+  $.get('/api/search/music')
     .then((resources) => {
       renderMusicResources(resources);
     })
@@ -58,93 +131,21 @@ $(() => {
   };
   }
 
-  if (window.location.pathname === '/search/Food%20and%20Drink') {
-  $.get('/api/search/Food%20and%20Drink')
-  .then((resources) => {
-    renderFoodAndDrinkResources(resources);
-  })
-  .catch(err => {
-    console.log('err:', err.message)
-  })
+  if (window.location.pathname === '/search/other') {
+    $.get('/api/search/other')
+    .then((resources) => {
+      renderOtherResources(resources);
+    })
+    .catch(err => {
+      console.log('err:', err.message)
+    })
 
-const renderFoodAndDrinkResources = (resources) => {
-  const $foodAndDrinkResourceContainer = $('#categorizedResources');
-
-  resources.reverse().forEach(resource => {
-    $foodAndDrinkResourceContainer.append(createResource(resource));
-  })
-};
-  }
-
-  if (window.location.pathname === '/search/Arts%20and%20Crafts') {
-    $.get('/api/search/Arts%20and%20Crafts')
-      .then((resources) => {
-        renderArtsAndCraftsResources(resources);
-      })
-      .catch(err => {
-        console.log('err:', err.message)
-      })
-
-    const renderArtsAndCraftsResources = (resources) => {
-      const $artsAndCraftsResourceContainer = $('#categorizedResources');
+    const renderOtherResources = (resources) => {
+      const $otherResourceContainer = $('#categorizedResources');
 
       resources.reverse().forEach(resource => {
-        $artsAndCraftsResourceContainer.append(createResource(resource));
+        $otherResourceContainer.append(createResource(resource));
       })
     };
-    }
-
-    if (window.location.pathname === '/search/Education') {
-      $.get('/api/search/Education')
-        .then((resources) => {
-          renderEducationResources(resources);
-        })
-        .catch(err => {
-          console.log('err:', err.message)
-        })
-
-      const renderEducationResources = (resources) => {
-        const $educationResourceContainer = $('#categorizedResources');
-
-        resources.reverse().forEach(resource => {
-          $educationResourceContainer.append(createResource(resource));
-        })
-      };
-      }
-
-      if (window.location.pathname === '/search/Animals') {
-        $.get('/api/search/Animals')
-          .then((resources) => {
-            renderAnimalsResources(resources);
-          })
-          .catch(err => {
-            console.log('err:', err.message)
-          })
-
-        const renderAnimalsResources = (resources) => {
-          const $animalsResourceContainer = $('#categorizedResources');
-
-          resources.reverse().forEach(resource => {
-            $animalsResourceContainer.append(createResource(resource));
-          })
-        };
-        }
-
-        if (window.location.pathname === '/search/Other') {
-          $.get('/api/search/Other')
-            .then((resources) => {
-              renderOtherResources(resources);
-            })
-            .catch(err => {
-              console.log('err:', err.message)
-            })
-
-          const renderOtherResources = (resources) => {
-            const $otherResourceContainer = $('#categorizedResources');
-
-            resources.reverse().forEach(resource => {
-              $otherResourceContainer.append(createResource(resource));
-            })
-          };
-          }
+  }
 });
