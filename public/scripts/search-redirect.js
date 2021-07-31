8f97fde36392d7b734c9256c3b043926cb8806b4
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-31 02:46:09 +0000

    add conditionals to transform category to match routes

--- public/scripts/search-redirect.js
+++ public/scripts/search-redirect.js
@@ -1,6 +1,26 @@
 $(() => {
   $("#search").change(function() {
-    window.location.href = `/search/${$(this).val()}`
+    let redirect;
+    if($(this).val() === 'Animals') {
+      redirect = 'animals';
+    }
+    if($(this).val() === 'Arts and Crafts') {
+      redirect = 'arts-and-crafts';
+    }
+    if($(this).val() === 'Education') {
+      redirect = 'education';
+    }
+    if($(this).val() === 'Food and Drink') {
+      redirect = 'food-and-drink';
+    }
+    if($(this).val() === 'Music') {
+      redirect = 'music';
+    }
+    if($(this).val() === 'Other') {
+      redirect = 'other';
+    }
+
+    window.location.href = `/search/${redirect}`
   })
 })
 
