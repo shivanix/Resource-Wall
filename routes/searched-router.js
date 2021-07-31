eaff898eda44015951c65a5b94efa2c9c90b47a9
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-31 02:50:57 +0000

    alphabetize and fix url character case

--- routes/searched-router.js
+++ routes/searched-router.js
@@ -4,89 +4,90 @@ const router  = express.Router();
 module.exports = (db) => {
 
   //Need to sql inject onclick category
-  router.get("/Music", (req, res) => {
-    db.query(`SELECT * FROM resources
-    WHERE category = $1;`, ['Music'])
-      .then(results => {
-        const resources = results.rows;
-        res.json( resources );
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  router.get("/Food%20and%20Drink", (req, res) => {
-    db.query(`SELECT * FROM resources
-    WHERE category = $1;`, ['Food and Drink'])
-      .then(results => {
-        const resources = results.rows;
-        res.json( resources );
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  router.get("/Arts%20and%20Crafts", (req, res) => {
-    db.query(`SELECT * FROM resources
-    WHERE category = $1;`, ['Arts and Crafts'])
-      .then(results => {
-        const resources = results.rows;
-        res.json( resources );
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  router.get("/Education", (req, res) => {
-    db.query(`SELECT * FROM resources
-    WHERE category = $1;`, ['Education'])
-      .then(results => {
-        const resources = results.rows;
-        res.json( resources );
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  router.get("/Animals", (req, res) => {
+  router.get("/animals", (req, res) => {
     db.query(`SELECT * FROM resources
     WHERE category = $1;`, ['Animals'])
-      .then(results => {
-        const resources = results.rows;
-        res.json( resources );
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    .then(results => {
+      const resources = results.rows;
+      res.json( resources );
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
   });
 
-  router.get("/Other", (req, res) => {
+
+  router.get("/arts-and-crafts", (req, res) => {
     db.query(`SELECT * FROM resources
-    WHERE category = $1;`, ['Other'])
+    WHERE category = $1;`, ['Arts and Crafts'])
+    .then(results => {
+      const resources = results.rows;
+      res.json( resources );
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
+  });
+
+  router.get("/education", (req, res) => {
+    db.query(`SELECT * FROM resources
+    WHERE category = $1;`, ['Education'])
+    .then(results => {
+      const resources = results.rows;
+      res.json( resources );
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
+  });
+
+  router.get("/food-and-drink", (req, res) => {
+    db.query(`SELECT * FROM resources
+    WHERE category = $1;`, ['Food and Drink'])
+    .then(results => {
+      const resources = results.rows;
+      res.json( resources );
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
+  });
+
+    router.get("/music", (req, res) => {
+      db.query(`SELECT * FROM resources
+      WHERE category = $1;`, ['Music'])
+        .then(results => {
+          const resources = results.rows;
+          res.json( resources );
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    });
+
+    router.get("/other", (req, res) => {
+      db.query(`SELECT * FROM resources
+      WHERE category = $1;`, ['Other'])
       .then(results => {
         const resources = results.rows;
         res.json( resources );
       })
       .catch(err => {
         res
-          .status(500)
-          .json({ error: err.message });
+        .status(500)
+        .json({ error: err.message });
       });
-  });
+    });
 
-  return router;
-};
+    return router;
+  };
